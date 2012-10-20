db92582044993091c37fe0f4e99687154ddf0480
Edmundo Carmona <eantoranz@gmail.com>
2012-10-20 11:16:02 -0430

    basic processing of NMEA 0183: GPGSA / GPRMC

--- src/gps/GpsDriver.java
+++ src/gps/GpsDriver.java
@@ -40,13 +40,42 @@ public class GpsDriver {
 			public void run() {
 				while (true) {
 					try {
-						System.out.println(input.readLine());
+						processInputLine(input.readLine());
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			}
 		}).start();
+	}
+	
+	private void processInputLine(String inputLine) {
+		if (inputLine.length() == 0) {
+			System.err.println("Empty input line");
+		}
+		if (inputLine.charAt(0) != '$') {
+			System.err.println("invalid input. Doesn't start with $ ("
+					+ inputLine + ")");
+		}
+		// FIXME incluir el checksum
+		
+		String [] fields = inputLine.substring(1).split(",");
+		if (fields[0].equals("GPGSA")) {
+			System.err.println("GPGSA");
+			if (fields[2].equals("1")) {
+				System.err.println("Fix is not available");
+			} else if (fields[2].equals("2")) {
+				System.out.println("2D");
+			} else if (fields[2].equals("3")) {
+				System.out.println("3D");
+			}
+		} else if (fields[0].equals("GPRMC")) {
+			System.err.println("GPRMC (Recommended Minimum)");
+			System.out.println("Time: " + fields[1] + " (UTC)");
+			System.out.println("Valid? " + (fields[2].equals("A") ? "Yes" : "No"));
+		} else {
+			System.err.println("Unknown input: " + inputLine);
+		}
 	}
 
 	public static void main(String[] args) {
