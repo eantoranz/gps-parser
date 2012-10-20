5073f4350e46a3886233c70ff7c2ba6409832344
Edmundo Carmona <eantoranz@gmail.com>
2012-10-20 06:21:06 -0430

    can read from serial port

--- src/gps/GpsDriver.java
+++ src/gps/GpsDriver.java
@@ -1,22 +1,23 @@
 package gps;
 
-import java.io.IOException;
-import java.util.EventListener;
-
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
 import gnu.io.NoSuchPortException;
 import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 import gnu.io.UnsupportedCommOperationException;
 
-public class GpsDriver {
+import java.io.IOException;
+import java.util.TooManyListenersException;
+
+public class GpsDriver implements SerialPortEventListener {
 
 	private SerialPort port;
 
 	public GpsDriver(String device, int speed) throws NoSuchPortException,
-			PortInUseException, IOException, UnsupportedCommOperationException {
+			PortInUseException, IOException, UnsupportedCommOperationException, TooManyListenersException {
 		CommPortIdentifier portId = CommPortIdentifier
 				.getPortIdentifier(device);
 
@@ -31,7 +32,8 @@ public class GpsDriver {
 		port = (SerialPort) theCommPort;
 		// now, let's set speed
 		port.setSerialPortParams(speed, 8, 1, SerialPort.PARITY_NONE);
-		
+		port.notifyOnDataAvailable(true);
+		port.addEventListener(this);
 	}
 	
 
@@ -41,6 +43,24 @@ public class GpsDriver {
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
+		}
+	}
+
+
+	public void serialEvent(SerialPortEvent event) {
+		// there was an event at the serial port
+		byte chars [] = new byte[1024];
+		
+		if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			// there's some data available at the port
+			try {
+				int bytesRead = port.getInputStream().read(chars, 0, 1024);
+				System.out.println("Read " + new String(chars));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.print("Some kind of port event: " + event);
 		}
 	}
 
