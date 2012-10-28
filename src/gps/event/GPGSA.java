4b7bbdbd318b214ea263a35bc6c4cf9146625d0f
Edmundo Carmona <eantoranz@gmail.com>
2012-10-28 16:21:29 -0430

    GPGSA: parse modes

--- src/gps/event/GPGSA.java
+++ src/gps/event/GPGSA.java
@@ -1,5 +1,7 @@
 package gps.event;
 
+import java.awt.image.BufferStrategy;
+
 import org.apache.log4j.Logger;
 
 /*
@@ -8,19 +10,73 @@ import org.apache.log4j.Logger;
  * Released under the terms of Mozilla Public License 2.0
  */
 public class GPGSA extends GpsEvent {
-	
+
 	private static Logger log = Logger.getLogger(GPGSA.class);
-	
-	protected GPGSA(String [] fields) {
+
+	public static final int MODE_AUTOMATIC = 1;
+	public static final int MODE_MANUAL = 2;
+	public static final int MODE_UNKNOWN = -1;
+
+	public static final int FIX_UNAVAILABLE = 1;
+	public static final int FIX_2D = 2;
+	public static final int FIX_3D = 3;
+	public static final int FIX_UNKNOWN = -1;
+
+	private int mode;
+	private int fixMode;
+
+	protected GPGSA(String[] fields) {
 		super(fields);
 		log.debug("GPGSA (Dillution of precision / Active satellites)");
+
+		// mode
+		if (fields[1].equals("A")) {
+			this.mode = MODE_AUTOMATIC;
+		} else if (fields[1].equals("M")) {
+			this.mode = MODE_MANUAL;
+		} else {
+			this.mode = MODE_UNKNOWN;
+		}
+
 		if (fields[2].equals("1")) {
-			log.debug("\tFix is not available");
+			this.fixMode = FIX_UNAVAILABLE;
 		} else if (fields[2].equals("2")) {
-			log.debug("\t2D");
+			this.fixMode = FIX_2D;
 		} else if (fields[2].equals("3")) {
-			log.debug("\t3D");
+			this.fixMode = FIX_3D;
+		} else {
+			this.fixMode = FIX_UNKNOWN;
 		}
 	}
 
+	public String toString() {
+		StringBuffer temp = new StringBuffer("GPGSA mode: ");
+		switch (this.mode) {
+		case MODE_AUTOMATIC:
+			temp.append("Automatic");
+			break;
+		case MODE_MANUAL:
+			temp.append("Manual");
+			break;
+		default:
+			temp.append("Unknown");
+		}
+		temp.append(" (" + this.mode + ") Fix Mode: ");
+		switch (this.fixMode) {
+		case FIX_2D:
+			temp.append("2D");
+			break;
+		case FIX_3D:
+			temp.append("3D");
+			break;
+		case FIX_UNAVAILABLE:
+			temp.append("Unavailable");
+			break;
+		default:
+			temp.append("Unknown");
+		}
+		temp.append(" (" + this.fixMode + ")");
+
+		return temp.toString();
+	}
 }
